1cfb2dca09b9382416cb14cef614f332fc22975b
Lyeuiechang <lyetonyemail@gmail.com>
2017-03-15 20:17:52 +0800

    Big Changes:Replace Project With Bootstrap Initialized.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
-window.onload = function() {
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", function() {
-        document.body.style.background = randColor();
-    });
-};
+// Google Analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-function randColor() {
-    var hex = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
-    return '#' + (function co(lor) {
-        return (lor += hex[Math.floor(Math.random() * 16)]) && (lor.length == 6)
-            ? lor
-            : co(lor);
-    })('');
-}
+// TODO add your tracking id here
+ga('create', 'tracking-id', 'auto');
+ga('send', 'pageview');
